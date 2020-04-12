54a613223c0e4c308bbbca0f017c18160f3303a5
Johanes Steven <johanessteven19@gmail.com>
2020-04-12 22:40:03 +0700

    Fixing settings.py

--- story7/settings.py
+++ story7/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,4 +119,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
+
+import django_heroku
+if '/app' in os.environ['HOME']:
+    django_heroku.settings(locals())
+
+import dj_database_url
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {'default': dj_database_url.config()}
