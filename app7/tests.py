f7d7fb82d62210999740170e52fd5d8b594b69f0
Johanes Steven <johanessteven19@gmail.com>
2020-04-12 23:12:09 +0700

    [RED] Testing url function 1st time

--- app7/tests.py
+++ app7/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase, Client
 from django.urls import resolve
-
+from .views import home
 
 # Create your tests here.
 class Story7Test(TestCase):
     def test_url_exist(self):
         response = Client().get('')
         self.assertEqual(response.status_code,200)
+    
+    def test_story7_using_index_func(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
